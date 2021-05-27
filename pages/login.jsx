f9eba81b44f8af205242bb598516714519f60bdc
Adrian <adrianalexander17@gmail.com>
2021-05-26 20:13:15 -0400

    finished first revision of login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,7 +3,13 @@ import Header from "@components/Header";
 import { getProviders, signIn } from "next-auth/client";
 import { useRouter } from "next/router";
 import Loading from "@components/Loading";
-import {useSession} from "next-auth/client";
+import { useSession } from "next-auth/client";
+import Image from "next/image";
+
+export const logos = {
+  Google: "/Google.svg",
+  GitHub: "/GitHub.svg"
+}
 
 const Login = ({ providers }) => {
   const router = useRouter();
@@ -13,12 +19,22 @@ const Login = ({ providers }) => {
   if (session) router.push("/");
   if (loading) return <Loading />;
   return (
-    <div>
+    <div className="bg-gray-900 min-h-screen flex justify-center items-center flex-col">
       <Header title="Login" />
       <>
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
+        {Object.values(providers).map((provider) =>
+
+        (
+          <div key={provider.name} className="box-border h-16 w-64 border-4 text-right bg-gray-800 grid grid-cols-3 flex space-x-0">
+            <div className="flex items-left pl-5 w-16">
+              <Image
+                width={25}
+                height={25}
+                src={logos[provider.name]}
+              />
+            </div>
+
+            <button className="text-white w-auto col-span-2" onClick={() => signIn(provider.id)}>
               Sign in with {provider.name}
             </button>
           </div>
