8cf4b81c2399942eb6875c1598153d03251d5c47
Sxyph <72323809+Sxyph@users.noreply.github.com>
2021-06-11 17:09:09 -0400

    a

---
+++ pages/comparison.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function comparison() {
+    return (
+        <div className="bg-gray-900 flex-1 flex justify-center items-center flex-col gap-6">
+            
+        </div>
+    )
+}
